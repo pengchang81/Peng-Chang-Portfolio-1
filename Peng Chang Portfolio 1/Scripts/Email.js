3facb376f1bdc0578f9f72477d8d1d76fdc41595
Peng Chang <pengchang81@yahoo.com>
2019-12-17 11:45:35 -0500

    Fixed Palindrome
    Fixed Palindrome code button

--- Peng Chang Portfolio 1/Scripts/Email.js
+++ Peng Chang Portfolio 1/Scripts/Email.js
@@ -1,11 +1,12 @@
-﻿$("#emailsubmit").click(function () {
-    if ($("#email").val() && $("#subject").val() && $("#emmessage").val() && $("#name").val() != null) {
+﻿function SendEmail() {
+    if ($("#cemail").val() && $("#csubject").val() && $("#cemmessage").val() && $("#cname").val() != null) {
         Email.send({
-            SecureToken: "f919f1b4-7c88-4809-b61b-969889efc85d",
+            SecureToken: "8f08b458-7243-4f07-a13e-a97bacb1a9d5",
+
             To: 'pengchang81@yahoo.com',
-            From: $("#email").val(),
-            Subject: $("#subject").val(),
-            Body: "From " + $("#name").val() + ": <BR> <BR> " + "Message:" + $("#emmessage").val()
+            From: $("#cemail").val(),
+            Subject: $("#csubject").val(),
+            Body: "From " + $("#cname").val() + ": <BR> <BR> " + "Message:" + $("#cemmessage").val()
         }).then(
            $("sendmessage").addClass("show")
         
@@ -14,4 +15,9 @@
     else {
         $("errormessage").addClass("show");
     }
-});
+};
+
+//Dan Token
+//384fcf19-3202-46d3-ad60-19643ed4f02d
+//My Token
+//f919f1b4-7c88-4809-b61b-969889efc85d
